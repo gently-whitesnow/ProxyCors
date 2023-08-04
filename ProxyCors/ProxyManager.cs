884ffa12fbfee6c4306ef4a04d494115ac9aa33e
getnly <gently.whitesnow@outlook.com>
2023-08-04 08:02:28 +0300

    fix query in proxy request

--- ProxyCors/ProxyManager.cs
+++ ProxyCors/ProxyManager.cs
@@ -6,7 +6,7 @@ namespace ProxyCors;
 public class ProxyManager
 {
     private readonly ProxyAdapter _proxyAdapter;
-
+    
     public ProxyManager(ProxyAdapter proxyAdapter)
     {
         _proxyAdapter = proxyAdapter;
@@ -14,15 +14,11 @@ public class ProxyManager
 
     public async Task<IActionResult> GetProxyResponseAsync(HttpRequest request)
     {
-        var requestString = request.Query["url"].ToString();
-
-        if (string.IsNullOrEmpty(requestString))
-            return new JsonResult("URL parameter is missing.")
-            {
-                StatusCode = 400
-            };
+        var queryString = request.QueryString.ToString();
+        var unescapedRequest = Uri.UnescapeDataString(queryString.Substring(1, queryString.Length - 1));
         
-        var response = await _proxyAdapter.GetProxyResponseAsync(requestString);
+        var response = await _proxyAdapter.GetProxyResponseAsync(unescapedRequest);
+        
         if (response.StatusCode == HttpStatusCode.InternalServerError)
             return new JsonResult("Internal server error")
             {
